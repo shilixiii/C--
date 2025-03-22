f9c3a7f0294020607ce180794e02bd426a104db9
shilixiii <shilixisy@gmail.com>
2025-03-22 17:19:25 +0800

    9

---
+++ 5.c
@@ -0,0 +1,20 @@
+//int main()
+//{
+//	int a, b, c;
+//	scanf("%d%d%d", &a, &b, &c);
+//	int d;
+//	d = a > b ? a : b;
+//	d = c > d ? c : d;
+//	printf("最大值是：%d\n", d);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 1;
+//	for (int i = 2; i <= 5; ++i)
+//	a = a * i;
+//	printf("%d\n", a);
+//	return 0;
+//}
