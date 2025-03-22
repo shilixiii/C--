a0e776cd1b71f3c2580db94b9aad883a42f0b00e
shilixiii <shilixisy@gmail.com>
2025-03-22 17:54:03 +0800

    1

---
+++ 1.c
@@ -0,0 +1,74 @@
+//#include <stdio.h>
+//int main()#include <stdio.h>
+//int main()
+//{
+//	short a = 10;
+//	int b = 100;
+//	long c = 1000L;
+//	printf("short:%hd\nint:%d\nlong:%ld\n", a, b, c);
+//	return 0;
+//}
+//{
+//	printf("hb\n");
+//	return 0;
+//}
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int a;
+//	scanf_s("%d", &a);
+//	printf("输出的数字为%d\n", a);
+//	return 0;
+//}
+
+//int Max(int x, int y)
+//{
+//	if (x >= y)
+//		return x;
+//	else
+//		return y;
+//}
+//
+//
+//int main()
+//{
+//	int Max(int x, int y);
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	int c = Max(a, b);
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	float n;
+//	printf(":::");
+//	scanf("%f", &n);
+//	printf("%f\n", n);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a, b, c;
+//	scanf("%d%d%d", &a, &b, &c);
+//	int d;
+//	d = a > b ? a : b;
+//	d = c > d ? c : d;
+//	printf("最大值是：%d\n", d);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 1;
+//	for (int i = 2; i <= 5; ++i)
+//	a = a * i;
+//	printf("%d\n", a);
+//	return 0;
+//}
