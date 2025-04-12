cf27e74a1130b725b57e9a9f5529772c3b9e89a8
shilixiii <shilixisy@gmail.com>
2025-04-12 20:05:53 +0800

    9

---
+++ 5.c
@@ -0,0 +1,9 @@
+//int main()
+//{
+//	for (int year = 2000;year <= 2111; ++year)
+//	{
+//		if (year % 4 == 0 && year % 4 != 100 || year % 400 == 0)
+//			printf("%d是闰年\n", year);
+//	}
+//	return 0;
+//}
