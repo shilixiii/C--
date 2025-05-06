a0f561d8746a83c376196d4d2193730f5ca997d3
shilixiii <shilixisy@gmail.com>
2025-05-06 20:45:09 +0800

    11

---
+++ 7.c
@@ -0,0 +1,47 @@
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	for (int i=2; i < n; ++i)
+//	{
+//		if (n % i == 0)
+//		{
+//			printf("%d不是素数\n", n);
+//			return 0;
+//		}
+//		
+//	}
+//	printf("%d是素数\n", n);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	for (int i = 2; i <= sqrt(n); ++i)
+//	{
+//		if (n % i == 0)
+//		{
+//			printf("%d不是素数\n", n);
+//			return 0;
+//		}
+//	}
+//	printf("%d是素数\n", n);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	double sum = 0;
+//	int sign = 1;
+//	for (int deno=1; deno <= 100; deno++)
+//	{
+//		sum += sign * 1.0/deno;
+//		sign *= -1;
+//	}
+//	printf("%lf", sum);
+//	return 0;
+//}
