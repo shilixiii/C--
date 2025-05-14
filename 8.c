b98f491e169321bedb3bf11ba007bb5f21b51805
shilixiii <shilixisy@gmail.com>
2025-05-14 20:37:59 +0800

    12

---
+++ 8.c
@@ -0,0 +1,42 @@
+//int main()
+//{
+//	int a;
+//	int max = 0;
+//	for (int i = 0; i < 10; ++i)
+//	{
+//		scanf("%d", &a);
+//		if (a > max)
+//			max = a;
+//	}
+//	printf("最大数为：%d", max);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a=0,b=0,c=0,t;
+//	/*printf("输入：%d%d%d", a, b, c);*/
+//	scanf("%d%d%d", &a, &b, &c);
+//	//确保a>=b，if函数的else忽略不写，表示为a>b
+//	if (a < b)
+//	{
+//		t = a;
+//		a = b;
+//		b = t;
+//	}
+//	if (a < c)
+//	{
+//		t = a;
+//		a = c;
+//		c = t;
+//	}
+//	if (b < c)
+//	{
+//		t = b;
+//		b = c;
+//		c = t;
+//	}
+//	printf("%d %d %d\n",a,b,c);
+//	return 0;
+//}
